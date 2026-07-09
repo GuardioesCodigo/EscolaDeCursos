dfc8ec75a5287e7ed3a23b5f4ebb2106041f39e3
buenoIago <iagocedup@gmail.com>
2026-07-09 19:49:28 -0300

    Adiciona projeto inicial

---
+++ EscolaDeCursos/EscolaDeCursos.Aplicacao/InjecaoDependencia.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EscolaDeCursos.Aplicacao;
+
+public static class InjecaoDependencia
+{
+    public static void AddApplicationServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+    }
+}
