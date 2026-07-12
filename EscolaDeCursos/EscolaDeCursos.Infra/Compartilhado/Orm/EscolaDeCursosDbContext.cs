8a406316b2b367d177f06b1ae02758a0d1056e7f
buenoIago <iagocedup@gmail.com>
2026-07-12 12:18:11 -0300

    feat: adiciona RepositorioBaseEmOrm

--- EscolaDeCursos/EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos/EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -1,4 +1,8 @@
 using System.Reflection;
+using EscolaDeCursos.Dominio.Modulos.ModuloAluno;
+using EscolaDeCursos.Dominio.Modulos.ModuloInstrutor;
+using EscolaDeCursos.Dominio.Modulos.ModuloMatricula;
+using EscolaDeCursos.Dominio.Modulos.ModuloTurma;
 using Microsoft.EntityFrameworkCore;
 
 namespace EscolaDeCursos.Infra.Compartilhado.Orm;
@@ -6,6 +10,11 @@ namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 public sealed class EscolaDeCursosDbContext(
     DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
 {
+    public DbSet<Aluno> Alunos => Set<Aluno>();
+    public DbSet<Instrutor> Instrutores => Set<Instrutor>();
+    public DbSet<Matricula> Matriculas => Set<Matricula>();
+    public DbSet<Turma> Turmas => Set<Turma>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
